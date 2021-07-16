5a8913c5573e4ac56475dc9772fe47d9efac9e39
Mightydevofficial <mightydevofficial@gmail.com>
2021-07-16 12:20:10 +0800

    MySOS Android Application Main Activity UPDATE

--- app/src/main/java/com/example/mysosapplication/Help.java
+++ app/src/main/java/com/example/mysosapplication/Help.java
@@ -1,13 +1,67 @@
 package com.example.mysosapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.security.PrivateKey;
 
 public class Help extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+
+        CustomWebViewClient client = new CustomWebViewClient(this);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(client);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://sites.google.com/view/mightydev/mysos-how-to-use?authuser=1");
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack()){
+            this.webView.goBack();
+            return true;
+
+        }
+        return super.onKeyDown(keyCode,event);
+
     }
 }
+
+class CustomWebViewClient extends WebViewClient {
+    private Activity activity;
+
+    public CustomWebViewClient(Activity activity) {
+        this.activity = activity;
+
+    }
+//API less than 24 Lolipop
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String Url) {
+        return false;
+    }
+//API more the 24 nugget
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request) {
+        return false;
+
+    }
+}
+
--- app/src/main/java/com/example/mysosapplication/Notes.java
+++ app/src/main/java/com/example/mysosapplication/Notes.java
@@ -1,29 +1,67 @@
 package com.example.mysosapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.security.PrivateKey;
 
 public class Notes extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    Adapter adapter;
+    private WebView webView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
-//get the list of notes title and content in string array
 
-        String[] titles = getResources().getStringArray(R.array.notes_title);
-        String[] content = getResources().getStringArray(R.array.notes_content);
-
-        recyclerView = findViewById(R.id.notesListView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new Adapter(this,titles,content); //our adapter takes two string array
-        recyclerView.setAdapter(adapter);
+        CustomWebViewClientNotes client = new CustomWebViewClientNotes(this);
+        webView1 = findViewById(R.id.webView1);
+        webView1.setWebViewClient(client);
+        webView1.getSettings().setJavaScriptEnabled(true);
+        webView1.loadUrl("https://sites.google.com/view/mightydev/notes?authuser=1");
 
 
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && this.webView1.canGoBack()){
+            this.webView1.goBack();
+            return true;
+
+        }
+        return super.onKeyDown(keyCode,event);
+
+    }
 }
+
+
+
+
+class CustomWebViewClientNotes extends WebViewClient {
+    private Activity activity;
+
+    public CustomWebViewClientNotes(Activity activity) {
+        this.activity = activity;
+
+    }
+    //API less than 24 Lolipop
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String Url) {
+        return false;
+    }
+    //API more the 24 nugget
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request) {
+        return false;
+
+    }
+}
+
